1611a92cb6229b42489d313503fde42a5fc10200
Subhajit Sahu <wolfram77@gmail.com>
2021-03-25 11:49:34 +0530

    :microbe: w/ init & setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="extra-boolean",
+  version='0.0.0',
+  author="Subhajit Sahu",
+  author_email="wolfram77@gmail.com",
+  description="Boolean data type has two possible truth values to represent logic.",
+  long_description="Boolean data type has two possible truth values to represent logic.",
+  keywords=['python', 'first package'],
+  classifiers= [
+    "Intended Audience :: Education",
+    "License :: OSI Approved :: MIT License"
+    "Operating System :: POSIX",
+    "Operating System :: POSIX :: BSD",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: MacOS :: MacOS X",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+  ],
+  packages=find_packages(),
+  install_requires=[]
+)
