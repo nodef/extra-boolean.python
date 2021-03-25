fa3bc95eb945d7d33c4cdde2784e7313ed145bd1
Subhajit Sahu <wolfram77@gmail.com>
2021-03-25 14:13:54 +0530

    :microbe: simplify setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
 from setuptools import setup
 
-with open("README.md", "r") as f:
-  long_description = f.read()
-
-setup(
-  # long_description=long_description,
-  # long_description_content_type="text/markdown"
-)
+setup()
