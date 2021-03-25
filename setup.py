e368605dbed389fafee4e45eb8f4bce4380b13d6
Subhajit Sahu <wolfram77@gmail.com>
2021-03-25 13:09:09 +0530

    :microbe: make pkg accessible

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+  name="extraboolean",
+  version='0.0.6',
+  author="Subhajit Sahu",
+  author_email="wolfram77@gmail.com",
+  description="Boolean data type has two possible truth values to represent logic.",
+  long_description="Boolean data type has two possible truth values to represent logic.",
+  keywords=['python', 'first package'],
+  classifiers= [
+    "Intended Audience :: Education",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: POSIX",
+    "Operating System :: POSIX :: BSD",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: MacOS :: MacOS X",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 3",
+  ],
+  packages=find_packages('extraboolean'),
+  install_requires=[]
+)
